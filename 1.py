0dd837fec29ebfa2b8b9a2cdec8da785e2784b53
Oleksii Aleksieiev <reneguarde@gmail.com>
2025-02-11 22:51:35 +0200

    Correct first task

--- 1.py
+++ 1.py
@@ -16,7 +16,7 @@ def find_min_max(
 
 
 def split_find_min_max(arr: List[int]) -> Tuple[int, int] | None:
-    if arr == []:
+    if not arr:
         return None
     if len(arr) == 1:
         return arr[0], arr[0]
@@ -28,5 +28,5 @@ def split_find_min_max(arr: List[int]) -> Tuple[int, int] | None:
 if __name__ == "__main__":
     arr = [randrange(-1000, 1000) for _ in range(100)]
     print("(arr):", arr)
-    print("(fn(min), fn(max)):", (min(arr), max(arr)))
     print("(min, max):", split_find_min_max(arr))
+    print("(min, max) verification:", (min(arr), max(arr)))
